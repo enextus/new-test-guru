aca586837152830101af3cf44fa7199b4a21b795
enextus <enextus@gmail.com>
2019-06-25 22:55:35 +0300

    listing_by_category rev

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,6 @@
 class Test < ApplicationRecord
   def self.listing_by_category(category_name)
-      Test.where(category_id: Category.where(title: category_name).ids).order(title: :desc).pluck(:title)
+    ids = Category.where(title: category_name).ids
+    Test.where("category_id = :category_id", category_id: ids).order(title: :desc).pluck(:title)
   end
 end
