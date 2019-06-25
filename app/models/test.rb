4229ee45d6cc141597659c5388890087ec0b9dcd
enextus <enextus@gmail.com>
2019-06-25 22:49:13 +0300

    class method listing_by_category

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,5 @@
 class Test < ApplicationRecord
+  def self.listing_by_category(category_name)
+      Test.where(category_id: Category.where(title: category_name).ids).order(title: :desc).pluck(:title)
+  end
 end
