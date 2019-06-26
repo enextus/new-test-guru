0b5ef158cc7927d4a0b86b3e95075092506fa1b7
enextus <enextus@gmail.com>
2019-06-26 09:10:42 +0300

    comment rev

--- app/models/test.rb
+++ app/models/test.rb
@@ -4,11 +4,3 @@ class Test < ApplicationRecord
     Test.where("category_id = :category_id", category_id: ids).order(title: :desc).pluck(:title)
   end
 end
-
-# irb(main):012:0> pp Test.tests_listing_by_category_name('Frontend')
-#    (0.6ms)  SELECT "categories"."id" FROM "categories" WHERE (title = 'Frontend')
-#    (0.8ms)  SELECT "tests"."title" FROM "tests" WHERE (category_id = 1) ORDER BY "tests"."title" DESC
-# ["3. Test for category: Frontend",
-#  "2. Test for category: Frontend",
-#  "1. Test for category: Frontend"]
-# => ["3. Test for category: Frontend", "2. Test for category: Frontend", "1. Test for category: Frontend"]
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  def level_check(level)
+    @level = level
+    puts level
+  end
+
+  def tests_by_level(level)
+    tests.where('level = :level', level: level )
+  end
 end
