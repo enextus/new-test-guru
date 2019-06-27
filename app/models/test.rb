590f92856c15a1aa48b30cd9aae0096003d02f19
enextus <enextus@gmail.com>
2019-06-27 16:57:20 +0300

    methods listing_by_category_title rev

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,5 @@
 class Test < ApplicationRecord
-  def self.listing_by_category_title(category_name)
-    Test.joins("INNER JOIN categories ON tests.category_id = categories.id").where("categories.title = :category_name", category_name: category_name).order(title: :desc).pluck(:title)
+  def self.listing_by_category_title(category_title)
+    Test.joins("INNER JOIN categories ON tests.category_id = categories.id").where("categories.title = :category_title", category_title: category_title).order(title: :desc).pluck(:title)
   end
 end
