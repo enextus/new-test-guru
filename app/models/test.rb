5c2aa57fe9e0d97d4893d86bc1f47b54fbb62b9a
enextus <enextus@gmail.com>
2019-06-27 15:07:32 +0300

    method listing_by_category_name rev

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,12 +1,12 @@
 class Test < ApplicationRecord
   def self.listing_by_category_name(category_name)
-
-
+    Test.joins("INNER JOIN categories ON tests.category_id = categories.id AND categories.title = '#{category_name}' ORDER BY tests.title DESC").pluck(:title)
   end
 end
 
 # SELECT tests.title
 # FROM categories
+
 # INNER JOIN tests
 # ON tests.category_id = categories.id AND categories.title = 'Frontend'
 # ORDER BY tests.title DESC;
