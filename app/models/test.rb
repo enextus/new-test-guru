f3a7786cc0e9b6440b4eb24dcd8ff6252016d2f8
enextus <enextus@gmail.com>
2019-06-27 13:33:05 +0300

    class method name changed

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,6 @@
 class Test < ApplicationRecord
   def self.listing_by_category_name(category_name)
-    ids = Category.where("title = :title", title: category_name).ids
-    Test.where("category_id = :category_id", category_id: ids).order(title: :desc).pluck(:title)
+    ids = Category.where(title: category_name).ids
+    Test.where(category_id: ids).order(title: :desc).pluck(:title)
   end
 end
