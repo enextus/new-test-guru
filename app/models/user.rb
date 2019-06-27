c656c50edba43e50d7ae57cdbb9ad49cbac9806b
enextus <enextus@gmail.com>
2019-06-27 13:25:39 +0300

    test_by_level rev

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,10 @@ class User < ApplicationRecord
 
   def tests_by_level(level)
     user_id = self.id
-    ids_of_initiated_tests_of_this_user = InitiatedTest.where(user_id: user_id).pluck(:test_id)
-    arr = []
-    ids_of_initiated_tests_of_this_user.map do |id|
-      arr << Test.where("id = :id AND level = :level", id: id, level: level)
+    InitiatedTest.where(user_id: user_id).pluck(:test_id).map do |id|
+      Test.where("id = :id AND level = :level", id: id, level: level).first
     end
   end
 end
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
