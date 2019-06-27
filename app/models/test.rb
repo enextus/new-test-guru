6089e50a73d963877338e91fb065d350807b82d4
enextus <enextus@gmail.com>
2019-06-27 16:23:25 +0300

    method tests_by_level rev

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,5 @@
 class Test < ApplicationRecord
   def self.listing_by_category_name(category_name)
-    Test.joins("INNER JOIN categories ON tests.category_id = categories.id AND categories.title = '#{category_name}' ORDER BY tests.title DESC").pluck(:title)
+    Test.joins("INNER JOIN categories ON tests.category_id = categories.id").where("categories.title = :category_name", category_name: category_name).order(title: :desc).pluck(:title)
   end
 end
