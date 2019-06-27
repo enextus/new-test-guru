d27ccf54fb572cc4da16bdaebc38058557e9373e
enextus <enextus@gmail.com>
2019-06-27 16:01:36 +0300

    methods  rev

--- app/models/test.rb
+++ app/models/test.rb
@@ -3,16 +3,3 @@ class Test < ApplicationRecord
     Test.joins("INNER JOIN categories ON tests.category_id = categories.id AND categories.title = '#{category_name}' ORDER BY tests.title DESC").pluck(:title)
   end
 end
-
-# SELECT tests.title
-# FROM categories
-
-# INNER JOIN tests
-# ON tests.category_id = categories.id AND categories.title = 'Frontend'
-# ORDER BY tests.title DESC;
-#              title
-# --------------------------------
-#  3. Test for category: Frontend
-#  2. Test for category: Frontend
-#  1. Test for category: Frontend
-# (3 rows)
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,23 +3,3 @@ class User < ApplicationRecord
       Test.joins("INNER JOIN initiated_tests ON initiated_tests.test_id = tests.id AND tests.level = #{level}").pluck(:title)
   end
 end
-
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-
-# SELECT tests.title
-# FROM tests
-# INNER JOIN initiated_tests
-# ON initiated_tests.test_id = tests.id AND tests.level = 3
-# ;
-#              title
-# --------------------------------
-#  1. Test for category: Frontend
-#  1. Test for category: Frontend
-#  1. Test for category: Frontend
-#  Test for category: Backend
-#  1. Test for category: Frontend
-#  Test for category: Backend
-#  2. Test for category: Frontend
-#  Test for category: Backend
-#  2. Test for category: Frontend
-# (9 rows)
