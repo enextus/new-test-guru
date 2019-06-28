7fdcdb56e44a6aadd46e05bde8f7c6d858435389
enextus <enextus@gmail.com>
2019-06-28 17:52:33 +0300

    level: level rev

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,7 @@
 class User < ApplicationRecord
   def tests_by_level(level = 0)
 
-    # puts "level: #{level}"
     # h = { level: level }
-    # pp "hash: #{h}"
     # pp h[:level]
 
       Test.joins("INNER JOIN initiated_tests ON initiated_tests.test_id = tests.id").where(level: level)
