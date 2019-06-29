7016a951af91dd1ab12812f426c1658488ff21b8
enextus <enextus@gmail.com>
2019-06-30 02:29:25 +0300

    add column progress to test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_26_102254) do
+ActiveRecord::Schema.define(version: 2019_06_29_231912) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2019_06_26_102254) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "progress", default: 0, null: false
     t.index ["test_id"], name: "index_initiated_tests_on_test_id"
     t.index ["user_id"], name: "index_initiated_tests_on_user_id"
   end
