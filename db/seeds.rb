46f080ef93361e75c594dba427f6d9ea085a780b
enextus <enextus@gmail.com>
2019-06-30 02:51:22 +0300

    seed rev

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-# #################################################################
-
 users = [
   { name: 'admin', admin: true },
   { name: 'adam' },
@@ -9,8 +5,6 @@ users = [
 ]
 
 User.create(users)
-
-# #################################################################
 
 categories = [
   { title: 'Frontend' },
@@ -20,28 +14,18 @@ categories = [
 
 categories = Category.create(categories)
 
-# #################################################################
-
 tests = categories.map do |category|
   Test.create(title: "Test for category: #{category.title}", level: [1, 2, 3].sample, category_id: category.id)
 end
-
-# #################################################################
 
 questions = tests.map do |test|
   Question.create(body: "Question for test: #{test.title}", test_id: test.id)
 end
 
-# #################################################################
-
 answers = questions.map do |question|
   Answer.create(body: "Answer for question: #{question.body}", question_id: question.id)
 end
 
-# #################################################################
-
 initiated_tests = tests.map do |test|
-  InitiatedTest.create(test_id: test.id, user_id: User.ids.sample, progress: [10, 30, 50 , 70, 100].sample)
+  InitiatedTest.create(test_id: test.id, user_id: User.ids.sample, progress: rand(1..100))
 end
-
-# #################################################################
