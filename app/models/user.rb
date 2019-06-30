48d6a4fa5f745087a9e8671d90151c39526b2538
enextus <enextus@gmail.com>
2019-06-30 14:05:29 +0300

    method tests_by_level rev

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   has_and_belongs_to_many :tests
 
   def tests_by_level(level)
-      Test.joins("INNER JOIN initiated_tests ON initiated_tests.test_id = tests.id").where(level: level)
+      Test.joins("INNER JOIN tests_users ON tests_users.test_id = tests.id").where(level: level)
   end
 end
