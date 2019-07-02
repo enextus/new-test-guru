438049c4e11348356bea6b13966c01ddb90b727f
enextus <enextus@gmail.com>
2019-07-02 23:31:15 +0300

    method created_tests rev

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,7 @@ class User < ApplicationRecord
   has_many :tests_users
   has_many :tests, through: :tests_users
 
-  has_many :created_tests
-
-  def created_tests
-    tests.find_by(user_id: self.id)
-  end
+  has_many :created_tests, class_name: "Test"
 
   def tests_by_level(level)
       Test.joins("INNER JOIN tests_users ON tests_users.test_id = tests.id").where(level: level)
