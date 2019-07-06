91a9147b72ce8f38d4d1b4cce7400d086364885a
enextus <enextus@gmail.com>
2019-07-06 15:03:38 +0300

    migrations ref

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_05_140042) do
+ActiveRecord::Schema.define(version: 2019_07_02_112229) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,9 +59,9 @@ ActiveRecord::Schema.define(version: 2019_07_05_140042) do
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.boolean "admin", default: false, null: false
+    t.string "email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "email", null: false
   end
 
   add_foreign_key "answers", "questions"
