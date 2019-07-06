c2fa4c9aca6fab5987432dc17d9ad260be325366
enextus <enextus@gmail.com>
2019-07-06 15:25:09 +0300

    model answers cor

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -9,6 +9,6 @@ class Answer < ApplicationRecord
   private
 
   def validate_max_answers_quantity
-    errors.add(:base, 'the maximum quantity of answers (4) for one question was reached') if Answer.where(question_id: question.id).count >= 4
+    errors.add(:base, 'the maximum quantity of answers (4) for one question was reached') if Answer.where(question_id: id).count >= 4
   end
 end
