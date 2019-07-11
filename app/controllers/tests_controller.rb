10bb2457dc7941e5799f1eaf988f63f0483a3c52
enextus <enextus@gmail.com>
2019-07-11 15:42:25 +0300

    mod rev

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,9 +1,19 @@
 class TestsController < ApplicationController
+
   def index
-    render plain: 'All tests'
+    # render plain: 'All tests'
+
+    # render html: "<h1>All tests </h1>".html_safe
+
+    # render json: { tests: Test.all }
+
+    # render inline: '<p>My favourite language is: <%= %[ybuR].reverse! %>!</p>'
+
+    # render file: 'public/hello', layout: false
+
+    # head 204
+
+    head :no_content
   end
 
-  def start
-    render plain: "Start certain test "
-  end
 end
---
+++ config/example_routes.rb
@@ -0,0 +1,40 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # resources :tests
+
+  # resource :account, controller: :account
+  # resources :tests, only: %i[index show]
+
+  # resources :tests do
+  #   resources :questions
+  # end
+
+  # resources :tests do
+  #   resources :questions, shallow: true
+  # end
+
+  # GET /tests/beginner
+  # POST /tests/1/start
+  # resources :tests do
+  #   get :beginner, on: :collection
+  #
+  #   member do
+  #     post :start
+  #   end
+  # end
+
+  # get '/tests', to: 'tests#index'
+  # get '/tests/:id', to: 'tests#show', as: :test
+
+  # get '/tests(/:id)', to: 'tests#show'
+  # get '/tests/:id/start', to: 'tests#start'
+  # get '/tests/:id/start', to: 'tasks#start'
+
+  # match '/tests/:id', to: 'tests#create', via: %i[post put]
+  # match '/tests/:id', to: 'tests#create', via: :all
+
+  get '/tests/:id/start', to: 'tests#start'
+  # get '/tests/:id/start', to: 'tasks#start'
+
+  root to: 'tests#index'
+end
