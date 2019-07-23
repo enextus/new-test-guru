c02f09815a36e829d6d8fada43c631f711108a72
enextus <enextus@gmail.com>
2019-07-23 17:37:50 +0300

    some changes

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,19 +1,33 @@
 class TestsController < ApplicationController
 
   def index
-    # render plain: 'All tests'
-
+    # pry
+    result = ["1.Class: #{params.class}", "2.Parameters: #{params.inspect}"]
+    render plain: result.join("\n")
+    # byebug
     # render html: "<h1>All tests </h1>".html_safe
-
     # render json: { tests: Test.all }
-
     # render inline: '<p>My favourite language is: <%= %[ybuR].reverse! %>!</p>'
-
     # render file: 'public/hello', layout: false
-
     # head 204
+    # head :no_content
+    # render inline: '<%= console %>'
 
-    head :no_content
+    # logger.info(self.object_id)
+    #
+    # respond_to do |format|
+    #   format.html { render plain: 'All tests' }
+    #   format.json { render json: { tests: Test.all } }
+    # end
   end
 
+  def show
+    redirect_to root_path
+    # redirect_to 'http://rubyonrails.org'
+  end
+
+  def new
+    # controller_name
+    # action_name
+  end
 end
