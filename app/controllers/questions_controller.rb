2ab8a8633e452478f49f6551eb4b6892d016d98d
enextus <enextus@gmail.com>
2019-07-26 10:57:56 +0300

    some rev

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,13 @@
 class QuestionsController < ApplicationController
+  before_action :find_questions, only: %i[index]
+
+  def index
+    render plain: @questions.join("\n")
+  end
+
+  private
+
+  def find_questions
+    @questions = Question.where(test_id: params[:test_id])
+  end
 end
