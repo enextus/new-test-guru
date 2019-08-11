563e49798a6d0f79a1bac5820e2f7c14283c8e7e
enextus <enextus@gmail.com>
2019-08-12 00:54:50 +0200

    rescue controller rev

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,6 @@
 class QuestionsController < ApplicationController
   before_action :find_questions, only: %i[index]
-  # rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
+
 
   def index
   end
